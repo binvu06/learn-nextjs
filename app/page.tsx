32b3c93dc8047cc3922fe75856edfeb1be79acda
binvu0602 <[vulegiahoa07]>
2024-07-17 14:05:03 +0700

    Change content

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <h1>Learn nextjs</h1>;
+  return (
+    <>
+      <h1>Learn nextjs</h1>;
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At, aliquid non? Similique a in
+        libero magnam ipsum asperiores neque quod nemo adipisci! Fuga voluptas amet accusantium
+        ratione nulla at dicta.
+      </p>
+    </>
+  );
 }
